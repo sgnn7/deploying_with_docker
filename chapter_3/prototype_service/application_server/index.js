34bc621fb9b0e1ae8adab4e124e5be724cd4f830
Srdjan Grubor <sgnn7@sgnn7.org>
2017-08-16 21:49:48 -0500

    Added a basic nodejs app server code
    This creates an app that can save and display words from a mongo
    database.

--- chapter_3/prototype_service/application_server/index.js
+++ chapter_3/prototype_service/application_server/index.js
@@ -1,14 +1,63 @@
 'use strict'
 
-const Express = require('express');
-const app = Express();
+const bodyParser = require('body-parser')
+const express = require('express');
+const mongo = require('mongodb')
 
+const DB_NAME = 'word_database';
+const DB_HOST = process.env.DB_HOST || 'localhost:27017';
+const COLLECTION_NAME = 'words';
 const SERVER_PORT = 8000;
 
-app.get('/ping', (req, res) => {
-      res.status(200).json({ success: true });
+const app = express();
+const db = mongo.MongoClient();
+const dbUri = `mongodb://${DB_HOST}/${DB_NAME}`;
+const words = [];
+
+app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+function loadWords(callback) {
+    db.connect(dbUri, (err, db) => {
+        if (err) {
+            throw err;
+        }
+
+        db.collection(COLLECTION_NAME).find({})
+                                      .toArray((err, docs) => {
+            words.push.apply(words, docs.map(doc => doc.word));
+            callback();
+        });
+    });
+}
+
+app.get('/', (req, res) => {
+    res.render('index', { words: words });
 });
 
-app.listen(SERVER_PORT, () => {
-    console.info("Server started on port %d...", SERVER_PORT);
+app.post('/new', (req, res) => {
+    const word = req.body.word;
+
+    console.info(`Got word: ${word}`);
+    if (word) {
+        db.connect(dbUri, (err, db) => {
+            if (err) {
+                throw err;
+            }
+
+            db.collection(COLLECTION_NAME).insertOne({ word } ,() => {
+                db.close();
+                words.push(word);
+            });
+        });
+    }
+
+    res.redirect('/');
+});
+
+loadWords(() => {
+    console.info("Data loaded from database");
+    app.listen(SERVER_PORT, () => {
+        console.info("Server started on port %d...", SERVER_PORT);
+    });
 });
